5765b4d753d536bd2582d47989a16105925ace62
Kra1t <p4ankov@gmail.com>
2023-10-29 11:14:08 +0500

    on 2.html added flash slow-faster effect

--- src/scripts/2.js
+++ src/scripts/2.js
@@ -1,1 +1,16 @@
-document.querySelector( '.flash-text' ).classList.add('animate__animated', 'animate__flash',);
+document.querySelector( '.flash-text' ).classList.add( 'animate__animated', 'animate__flash', 'animate_slow', );
+setTimeout(() => {
+  document.querySelector( '.flash-text' ).classList.remove( 'animate__animated', 'animate__flash', 'animate_slow' )
+}, 1000);
+setTimeout(() => {
+  document.querySelector( '.flash-text' ).classList.remove( 'animate__animated', 'animate__flash', 'animate__slow' )
+}, 1100);
+setTimeout(() => {
+  document.querySelector( '.flash-text' ).classList.add( 'animate__animated', 'animate__flash', 'animate__fast')
+}, 1100);
+setTimeout(() => {
+  document.querySelector( '.flash-text' ).classList.remove( 'animate__animated', 'animate__flash', 'animate__fast')
+}, 1900);
+setTimeout(() => {
+  document.querySelector( '.flash-text' ).classList.add( 'animate__animated', 'animate__flash', 'animate__faster', 'animate__repeat-3')
+}, 1900);
