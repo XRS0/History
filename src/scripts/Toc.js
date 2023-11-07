81f64d67c9d8577bca421f9f6c982b92baed213c
Kra1t <p4ankov@gmail.com>
2023-11-07 15:41:56 +0500

    added script for flash-text

--- src/scripts/Toc.js
+++ src/scripts/Toc.js
@@ -1,14 +1,14 @@
-document.querySelector( '.buttons' || '.svg' ).classList.add( 'animate__animated', 'animate__flash', 'animate__fast', )
-document.querySelector( '.buttons' || '.svg' ).style.display = 'block'
+document.querySelector( '.buttons' || '.svg' || '.flash-text' ).classList.add( 'animate__animated', 'animate__flash', 'animate__fast', )
+document.querySelector( '.buttons' || '.svg' || '.flash-text' ).style.display = 'block'
 
 setTimeout(() => {
-  document.querySelector( '.buttons' || '.svg' ).classList.remove( 'animate__animated', 'animate__flash', 'animate__fast' )
+  document.querySelector( '.buttons' || '.svg' || '.flash-text' ).classList.remove( 'animate__animated', 'animate__flash', 'animate__fast' )
 }, 500);
 
 setTimeout(() => {
-  document.querySelector( '.buttons' || '.svg' ).classList.add( 'animate__animated', 'animate__flash', 'animate__faster', 'animate-repeat-2' )
+  document.querySelector( '.buttons' || '.svg' || '.flash-text' ).classList.add( 'animate__animated', 'animate__flash', 'animate__faster', 'animate-repeat-2' )
 }, 600);
 
 setTimeout(() => {
-  document.querySelector( '.buttons' || '.svg' ).style.display = 'block'
+  document.querySelector( '.buttons' || '.svg' || '.flash-text' ).style.display = 'block'
 }, 1000);
